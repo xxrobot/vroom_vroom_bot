a2996311cd1b200be7dd35478531a67a888dee7f
Scott Sasaki <scottsasaki@scottsasaki.local>
2016-03-13 01:07:40 -0800

    changed desert scene

--- config.py
+++
@@ -1,6 +0,0 @@
-cfg = { 
-	"consumer_key"        : "SECRETKEYZ",
-	"consumer_secret"     : "SECRETSECRETZ",
-	"access_token"        : "ACCESSTOEKENZ",
-	"access_token_secret" : "ACCESSTOKENSECRETS"
-}
